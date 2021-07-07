c82a5c76f26c9a70c4a18a64b61789d0dd82a551
gdfederici <gdfederici@gmail.com>
2021-07-07 11:35:15 +0200

    [ADD] index.js.

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+// IT_ Importare CSS. | EN_ Import CSS.
+import '../css/style.scss';
+// IT_ Importare funzioni. | EN_ Import functions.
+
+// IT_ Importare dati. | EN_ Import data.
+
+// IT_ Importare immagini. | EN_ Import images.
+
+/*** ------------------------- *** ------------------------- *** ------------------------- *** ------------------------- ***/
